ca49ce5666b03cbe378ad878579b97cd415437f8
py-task <2529716798@qq.com>
2017-12-30 22:30:14 +0800

    test

---
+++ ERPServer/src/test/java/service/dataservice/Impl/AdminDataServiceImplTest.java
@@ -0,0 +1,36 @@
+package service.dataservice.Impl;
+
+import org.junit.Test;
+import po.AdminPO;
+import service.dataservice.AdminDataService;
+
+import static org.junit.Assert.*;
+
+public class AdminDataServiceImplTest {
+    AdminDataService dataService = new AdminDataServiceImpl();
+    AdminPO po = new AdminPO();
+    @Test
+    public void add() throws Exception {
+    }
+
+    @Test
+    public void delete() throws Exception {
+    }
+
+    @Test
+    public void update() throws Exception {
+    }
+
+    @Test
+    public void getUsers() throws Exception {
+    }
+
+    @Test
+    public void find() throws Exception {
+    }
+
+    @Test
+    public void addAdvancedUser() throws Exception {
+    }
+
+}
